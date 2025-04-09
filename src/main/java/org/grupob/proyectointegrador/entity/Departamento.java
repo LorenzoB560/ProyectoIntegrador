9fcfab0bb2ce91e6f263cc4b9661517548040ee9
Iván Eliecer Bosquez Orna <132992272+IVA-BO@users.noreply.github.com>
2025-04-09 20:20:02 +0200

    INSERCION DE ENTIDADES

---
+++ src/main/java/org/grupob/proyectointegrador/entity/Departamento.java
@@ -0,0 +1,41 @@
+package org.grupob.proyectointegrador.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "departamento", uniqueConstraints = {
+        @UniqueConstraint(name = "UQ_departamento_codigo", columnNames = "codigo"),
+        @UniqueConstraint(name = "UQ_departamento_id_jefe", columnNames = "id_jefe"),
+})
+@NoArgsConstructor
+@AllArgsConstructor
+public class Departamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "codigo", unique = true, nullable = false)
+    private String codigo;
+
+    @Column(name = "nombre", unique = true, nullable = false)
+    private String nombre;
+
+    @Column(name = "localidad")
+    private String localidad;
+
+    @Column(name = "presupuesto", nullable = false)
+    private BigDecimal presupuesto;
+
+
+    @OneToMany(mappedBy = "departamento")
+    private List<Empleado> empleados = new ArrayList<>();
+}
