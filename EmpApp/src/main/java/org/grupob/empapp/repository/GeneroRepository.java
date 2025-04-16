52b426c0209cbd02274ce2be78fe351121b80569
Ismael Vega <vladerk.v@gmail.com>
2025-04-16 15:19:11 +0200

    Creación entidades maestras, actualización build.gradle de todos los módulos, creación ModelMapper y componente para la carga de datos maestros

---
+++ EmpApp/src/main/java/org/grupob/empapp/repository/GeneroRepository.java
@@ -0,0 +1,7 @@
+package org.grupob.empapp.repository;
+
+import org.grupob.empapp.entity.maestras.Genero;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GeneroRepository extends JpaRepository<Genero,  Long> {
+}
