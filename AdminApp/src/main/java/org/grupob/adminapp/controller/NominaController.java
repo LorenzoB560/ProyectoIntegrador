62530201d0fd690488cdbf26adbfc4b78279a9a5
LorenzoB560 <lorenzoboda560@gmail.com>
2025-05-06 19:19:46 +0200

    Detalle de nómina hecho

--- AdminApp/src/main/java/org/grupob/adminapp/controller/NominaController.java
+++ AdminApp/src/main/java/org/grupob/adminapp/controller/NominaController.java
@@ -3,13 +3,14 @@ package org.grupob.adminapp.controller;
 import org.grupob.adminapp.dto.NominaDTO;
 import org.grupob.adminapp.service.AltaNominaServiceImp;
 import org.grupob.adminapp.service.NominaServiceImp;
-import org.grupob.comun.entity.Nomina;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/nomina")
@@ -17,7 +18,7 @@ public class NominaController {
 
     private final NominaServiceImp nominaServiceImp;
 
-    public NominaController(NominaServiceImp nominaServiceImp) {
+    public NominaController(NominaServiceImp nominaServiceImp, AltaNominaServiceImp altaNominaServiceImp) {
         this.nominaServiceImp = nominaServiceImp;
     }
 
@@ -27,7 +28,16 @@ public class NominaController {
 
         List<NominaDTO> listaNominas = nominaServiceImp.devolverNominas();
         modelo.addAttribute("listaNominas", listaNominas);
-        return "nomina/listado-vista-nomina";
+        return "listados/listado-vista-nomina";
+    }
+
+    @GetMapping("/detalle/{id}")
+    public String vistaDetalleProducto(@PathVariable UUID id, Model model) {
+
+        //Obtengo el DTO de la nómina según el ID en el servicio, y se lo paso a la vista
+        model.addAttribute("nominaDTO", nominaServiceImp.devolverNominaPorId(id));
+
+        return "listados/detalle-vista-nomina";
     }
 
 }
--- AdminApp/src/main/java/org/grupob/adminapp/service/NominaServiceImp.java
+++ AdminApp/src/main/java/org/grupob/adminapp/service/NominaServiceImp.java
@@ -1,5 +1,6 @@
 package org.grupob.adminapp.service;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.grupob.adminapp.converter.NominaConverter;
 import org.grupob.adminapp.dto.NominaDTO;
 import org.grupob.comun.entity.Empleado;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,6 +25,17 @@ public class NominaServiceImp implements NominaService{
         this.nominaRepository = nominaRepository;
         this.nominaConverter = nominaConverter;
         this.empleadoRepository = empleadoRepository;
+    }
+    public NominaDTO devolverNominaPorId(UUID id){
+        Optional<Nomina> nomina = nominaRepository.findById(id);
+        if (nomina.isPresent()) {
+            NominaDTO nominaDTO = nominaConverter.convierteADTO(nomina.get());
+            Optional<Empleado> empleado = empleadoRepository.findById(nominaDTO.getIdEmpleado());
+            empleado.ifPresent(value -> nominaDTO.setNombre(value.getNombre() + " " + value.getApellido()));
+            return nominaDTO;
+        } else{
+            throw new EntityNotFoundException("La nómina seleccionada no existe");
+        }
     }
 
     public List<NominaDTO> devolverNominas(){
@@ -41,4 +54,5 @@ public class NominaServiceImp implements NominaService{
         System.out.println(nominasDTO);
         return nominasDTO;
     }
+
 }
