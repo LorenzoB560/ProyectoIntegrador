b056f401ad68789de8f934b21eb101263daf4c1f
Ismael Vega <vladerk.v@gmail.com>
2025-05-15 00:03:13 +0200

    añadido empaquetado war para despliegue en tomcat

--- EmpApp/src/main/java/org/grupob/empapp/EmpAppApplication.java
+++ EmpApp/src/main/java/org/grupob/empapp/EmpAppApplication.java
@@ -3,6 +3,8 @@ package org.grupob.empapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
@@ -10,10 +12,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages = {"org.grupob.comun","org.grupob.empapp"})
 @EnableJpaRepositories(basePackages = "org.grupob.comun.repository")
 @EntityScan(basePackages = "org.grupob.comun.entity")
-public class EmpAppApplication {
+public class EmpAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpAppApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EmpAppApplication.class);
+	}
 }
