85c63e797c22878d5ae5a88c0872bfaf68bc2346
Iván Eliecer Bosquez Orna <132992272+IVA-BO@users.noreply.github.com>
2025-05-14 12:44:51 +0200

    cambios en la vista departamentos y pruductos

--- EmpApp/src/main/resources/static/js/detalle-dep.js
+++ EmpApp/src/main/resources/static/js/detalle-dep.js
@@ -36,29 +36,12 @@ function mostrarDetalleDepartamento(departamento) {
     document.getElementById('id').textContent = departamento.id;
     document.getElementById('codigo').textContent = departamento.codigo;
     document.getElementById('nombre').textContent = departamento.nombre;
-    document.getElementById('localizacion').textContent = departamento.localizacion;
-    document.getElementById('direccion').innerHTML = formatearDireccion(departamento.direccion);
+    document.getElementById('localizacion').textContent = departamento.localidad;
 
     ocultarCargando();
     document.getElementById('detalleDepartamento').style.display = 'block';
 }
 
-// Función para formatear la dirección
-function formatearDireccion(direccion) {
-    if (!direccion) return 'No hay información de dirección';
-
-    return `
-                <div class="direccion-detalle">
-                    ${direccion.calle ? `${direccion.calle} ${direccion.numero}` : ''}<br>
-                    ${direccion.escalera ? `Escalera ${direccion.escalera}` : ''}
-                    ${direccion.piso ? `Piso ${direccion.piso}` : ''}
-                    ${direccion.letra ? `Letra ${direccion.letra}` : ''}<br>
-                    ${direccion.cp ? `CP: ${direccion.cp}` : ''}
-                    ${direccion.localidad ? `${direccion.localidad}, ` : ''}
-                    ${direccion.pais || ''}
-                </div>
-            `;
-}
 
 // Funciones de control de UI
 function mostrarCargando() {
