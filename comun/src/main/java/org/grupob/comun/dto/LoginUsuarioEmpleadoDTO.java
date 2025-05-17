a1d64b0f1d5c43154880def607a81f6c5b10950b
Ismael Vega <vladerk.v@gmail.com>
2025-05-17 17:10:45 +0200

    Solucionado mensaje de contraseña y errores tras el despliegue de empapp como ROOT en tomcat

--- comun/src/main/java/org/grupob/comun/dto/LoginUsuarioEmpleadoDTO.java
+++ comun/src/main/java/org/grupob/comun/dto/LoginUsuarioEmpleadoDTO.java
@@ -23,13 +23,13 @@ public class LoginUsuarioEmpleadoDTO {
     @EmailValidado(groups = GrupoUsuario.class)
     private String usuario;
 
-    @NotBlank(groups = GrupoClave.class)
+   /* @NotBlank(groups = GrupoClave.class)
     @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[.,;:¿?¡!(){}])[A-Za-z\\d.,;:¿?¡!(){}]{8,12}$",
             message = """
                     Introduzca una contraseña válida. Debe tener una longitud de entre 8 y 12 caracteres.\s
                      - Debe contener, al menos, una letra mayúscula, una minúscula, un número y un\s
                     signo de puntuación.""",
-            groups = GrupoClave.class)
+            groups = GrupoClave.class)*/
     private String clave;
 
     private Integer numeroAccesos;
